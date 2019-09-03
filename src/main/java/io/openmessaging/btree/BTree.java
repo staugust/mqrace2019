d2ab4a5c486661fc5440a15313d7a7dcf763ba2b
Jinghua.Yao <staugusto91@gmail.com>
2019-09-03 17:52:21 +0800

    haa

--- src/main/java/io/openmessaging/btree/BTree.java
+++ src/main/java/io/openmessaging/btree/BTree.java
@@ -2,6 +2,7 @@ package io.openmessaging.btree;
 
 import io.openmessaging.Message;
 
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,14 +13,16 @@ public class BTree {
   TreeNode root;
   TreeNode leftLeaf;
   TreeNode rightLeaf;
-  AtomicLong dataPos;
-  AtomicLong nodePos;
   long rootOffset;
   long leftLeafOffset;
   long rightLeafOffset;
 
   FileChannel treeF;
   FileChannel dataF;
+  AtomicLong dataPos;
+  AtomicLong nodePos;
+  AtomicLong dataFileSize = new AtomicLong(0);
+  AtomicLong treeFileSize = new AtomicLong(0);
 
   public BTree(int idx) {
     Path treePath = Paths.get(String.format(Constants.Tree_bin, idx));
@@ -29,14 +32,14 @@ public class BTree {
           StandardOpenOption.DSYNC, StandardOpenOption.SPARSE, StandardOpenOption.CREATE);
       dataF = FileChannel.open(dataPath, StandardOpenOption.READ, StandardOpenOption.WRITE,
           StandardOpenOption.DSYNC, StandardOpenOption.SPARSE, StandardOpenOption.CREATE);
-      if(treeF.size() < Constants.FileBlockSize){
+      if (treeF.size() < Constants.FileBlockSize) {
         treeF.truncate(Constants.FileBlockSize);
       }
-      if(dataF.size() < Constants.FileBlockSize){
+      if (dataF.size() < Constants.FileBlockSize) {
         dataF.truncate(Constants.FileBlockSize);
       }
 
-    }catch (Exception e){
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
@@ -49,15 +52,63 @@ public class BTree {
         root.capacity.getAndDecrement();
       } else {
       }
-    }else{
+    } else {
       // from the right most leaf node.
 
     }
   }
 
   public long writeData(Message msg) {
-    long pos = nodePos.addAndGet(34);
+
+    long pos = nodePos.addAndGet(Constants.MsgBodyLen);
     return pos;
   }
 
+  public Message readMessage(long offset, long t, long a) {
+    byte[] bytes = new byte[(int) Constants.MsgBodyLen];
+    try {
+      dataF.read(ByteBuffer.wrap(bytes), offset);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return new Message(a, t, bytes);
+  }
+
+  static class Test implements Runnable{
+    Integer x;
+    public Test(Integer i){
+      this.x = i;
+    }
+    @Override
+    public void run() {
+      synchronized (x) {
+        System.out.println(x);
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        System.out.println(x);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Integer x = 0;
+    Thread th1 = new Thread(new Test(x));
+    th1.start();
+    try {
+      Thread.sleep(500);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    x = new Integer(4);
+    System.out.println("main thread " + x);
+    try {
+      th1.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+  }
 }
